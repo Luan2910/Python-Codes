daf36ecf6bca6457c10c944cf8fe6d00fe5c9f40
Luan2910 <luanvictor2910@hotmail.com>
2020-04-01 15:51:54 -0300

    mudança 2

--- chat python/Client.py
+++ chat python/Client.py
@@ -11,7 +11,7 @@ IP = "191.52.64.119"
 #PORT = int(input("Porta: "))
 PORT = 5001
 
-#mudança
+#mudança 2
 
 my_username = input("Username: ")
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
